f0703c726334d5c3e37e8edb058e5624c6ae2f74
Engineer-kim <ddang0103@naver.com>
2024-11-22 17:09:32 +0900

    Refactor: 함수 분리
    API 엔드포인트 모아두는곳과
    각 엔드포인트 별 처리로직 따로 분리하여

---
+++ routes/events.go
@@ -0,0 +1,60 @@
+package routes
+
+import (
+	"Go-RestApi/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func getEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Fetching Data Fail"})
+		return
+	}
+	// HTTP 요청에 대한 응답을 직접 처리하는 역할하기때문에 리턴 안해도됨
+	context.JSON(http.StatusOK, events) //json  형태로 보내겟다 리턴을 안해도됨
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	event, err := models.GetEventByID(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could Not parse event id"})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
+}
+
+func createEvents(context *gin.Context) {
+	var event models.Event
+	// HTTP 요청 바디의 JSON 데이터를 event 구조체에 바인딩합니다.
+	// &event는 event 구조체의 메모리 주소를 의미하며,
+	// ShouldBindJSON 함수는 이 주소를 통해 JSON 데이터를 구조체에 직접 채워 넣습니다.
+	// 즉, 클라이언트에서 전달된 JSON 데이터의 각 필드가 event 구조체의 해당 필드에 매핑됩니다.
+	err := context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Parsing Error data"})
+		return
+	}
+	//하기의 코드는 추후 DB 연결 후 바꿀 예정
+	event.ID = 1
+	event.UserID = 1
+
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Save Data Fail"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Success Create", "event": event})
+}
